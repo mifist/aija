f6db16ecbd95cb1cdc257b5fea0c2e7005826303
Daria Moskalets <daria1992moskalets@gmail.com>
2023-01-15 21:02:23 +0200

    updated packeges; create new article view (locked)

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,17 +4,17 @@ import whatInput from 'what-input';
 window.$ = $;
 
 // lib
-import './lib/slick.min';
+import './lib/slick.min.js';
 
 
 // Bootstrap 5
-import './bootstrap/bootstrap.bundle.min';
+import './bootstrap/bootstrap.bundle.min.js';
 // components
-import './components/select2.min';
-import "./components/main";
-import "./components/custom";
-import "./components/podcastMedia";
-import "./components/chat";
+import './components/select2.min.js';
+import "./components/main.js";
+import "./components/custom.js";
+import "./components/podcastMedia.js";
+import "./components/chat.js";
 
 
 
