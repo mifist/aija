ad6ffd9f9aac925fbcae242909bbe7abec9ca2f9
Sergey <39513714+sulimka@users.noreply.github.com>
2023-10-31 11:43:09 +0200

    highlight search

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -10,8 +10,10 @@ import './lib/slick.min.js';
 // Bootstrap 5
 import './bootstrap/bootstrap.bundle.min.js';
 // components
+
 import './components/select2.min.js';
 import "./components/main.js";
+
 import "./components/custom.js";
 import "./components/podcastMedia.js";
 import "./components/chat.js";
